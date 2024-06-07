d295bc7388f9fccc87da040f60f4a9a5323996fd
Shreya00105 <raoshreya0130@gmail.com>
2024-06-07 14:37:05 +0530

    calculatorjs.js

--- calculatorjs.js
+++ calculatorjs.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
     const display = document.getElementById('display');
     const buttons = document.querySelectorAll('.btn');
